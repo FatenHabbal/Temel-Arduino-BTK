2a54dbb374ec53260ecb8a0efe8a5d66b0ab5c99
Faten Habbal <107545770+FatenHabbal@users.noreply.github.com>
2024-04-28 22:29:19 +0300

    lesson2.c
    Blink(Polis Işık)
    28/04/2024

---
+++ lesson2.c
@@ -0,0 +1,32 @@
+void setup (){
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+void loop (){
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  delay(500);
+  digitalWrite(3,0);
+  digitalWrite(4,0);
+  delay(500);
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  delay(500);
+  digitalWrite(3,0);
+  digitalWrite(4,0);
+  delay(500);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  delay(500);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+  delay(500);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  delay(500);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+  delay(500);
+}
