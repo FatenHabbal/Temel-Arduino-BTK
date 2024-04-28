44d3cbcfbc1bc0e07ce78d1235431749003491c7
Faten Habbal <107545770+FatenHabbal@users.noreply.github.com>
2024-04-28 22:23:24 +0300

    lesson1.c
    Blink(Traffic Light)

---
+++ lesson1.c
@@ -0,0 +1,22 @@
+void setup (){
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+void loop (){
+  digitalWrite(3,1);
+  delay(3000);
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  delay(2000);
+  digitalWrite(3,0);
+  digitalWrite(4,0);
+  digitalWrite(5,1);
+  delay(3000);
+  digitalWrite(5,1);
+  digitalWrite(4,1);
+  delay(2000);
+  digitalWrite(5,0);
+  digitalWrite(4,0);
+}
+0 comments on commit c607881
