468dfdf288b926251484429dc7942e16e6cf15c7
Faten Habbal <107545770+FatenHabbal@users.noreply.github.com>
2024-05-12 11:55:33 +0300

    week4.c
    lesson7-8

--- week4.c
+++ week4.c
@@ -73,7 +73,7 @@ void loop() {
 
 
 int mesafe;
-NewPing ultrasonikSensor (6,7,200);
+NewPing ultrasonikSensor (6,7,200); //(trigger_pin,echo_pin,max_mesafe)
 Servo myServo;
 void setup(){
   Serial.begin(9600);
@@ -83,7 +83,7 @@ void setup(){
 void loop(){
   mesafe = ultrasonikSensor.ping_cm();
 
-  if (mesafe<15)
+  if (mesafe<15 && mesafe>0) // && işareti ve anlamına gelir.
   {
     myServo.write(90);
   }
@@ -96,3 +96,139 @@ void loop(){
   delay(500);
 }
 
+######################### 
+
+#include <NewPing.h>
+#include <LiquidCrystal_I2C.h>
+
+NewPing uSensor(3,2,300);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int mesafe;
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+}
+
+void loop() {
+  mesafe = uSensor.ping_cm();
+  lcd.setCursor(0,0);
+  lcd.print("Mesafe= ");
+  lcd.setCursor(9,0);
+  lcd.print(mesafe);
+  delay(500);
+  lcd.clear();
+
+}
+
+
+
+
+############
+
+#include <NewPing.h>
+#include <LiquidCrystal_I2C.h>
+
+NewPing uSensor(3,2,300);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int mesafe;
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+}
+
+void loop() {
+  mesafe = uSensor.ping_cm();
+  lcd.setCursor(0,0);
+  lcd.print("Mesafe= ");
+  lcd.setCursor(8,0);
+  lcd.print(mesafe);
+  if(mesafe>40 && mesafe<50)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Yaklaştınız!");
+  }
+  else if (30<mesafe && mesafe<=40)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Çok Yaklaştınız!");
+  }
+  else if (0<mesafe && mesafe<=30)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Dikkat Et!");
+  }
+  else
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Güvenli");
+  }
+  delay(500);
+  lcd.clear();
+
+}
+
+
+
+
+##########
+
+
+#include <NewPing.h>
+#include <LiquidCrystal_I2C.h>
+
+NewPing uSensor(3,2,300);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int mesafe;
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  mesafe = uSensor.ping_cm();
+  lcd.setCursor(0,0);
+  lcd.print("Mesafe= ");
+  lcd.setCursor(8,0);
+  lcd.print(mesafe);
+  if(mesafe>40 && mesafe<50)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Yaklastiniz");
+    digitalWrite(5,1);
+    delay(1000);
+    digitalWrite(5,0);
+  }
+  else if (30<mesafe && mesafe<=40)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Cok Yaklastin");
+    digitalWrite(6,1);
+    delay(1000);
+    digitalWrite(6,0);
+  }
+  else if (0<mesafe && mesafe<=30)
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Dikkat Et!");
+    digitalWrite(7,1);
+    delay(1000);
+    digitalWrite(7,0);
+  }
+  else
+  {
+    lcd.setCursor(0,1);
+    lcd.print("Guvenli");
+  }
+  
+  delay(500);
+  lcd.clear();
+
+}
